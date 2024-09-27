8164b3b979eb4fffe7289c9215ca15db38f563a3
Torben2907 <torben.heckes@hhu.de>
2024-09-27 16:19:25 +0200

    refactor tests

--- tests/test_encode.py
+++ tests/test_encode.py
@@ -6,11 +6,13 @@ from encode import parse_biomed_data_to_ndarray
 
 
 def test_cervical_cancer_new_dataset():
-    X, y, df = parsing_helper("cervical_new", return_X_y=True)
+    X, y, df = parsing_helper("cervical_new")
     assert X.shape == (761, 7)
     assert y.shape == (761,)
     assert df.shape == (761, 8)
 
+def test_correct_parsing_of_start_and_end_of_file():
+    X, y, df = parsing_helper("cervical_new")
     np.testing.assert_allclose(X[0], np.array([52, 5, 16, 4, 1, 37, 37]))
     np.testing.assert_equal(y[0], 1)
     np.testing.assert_allclose(X[-1], np.array([29, 2, 20, 1, 0, 0, 0]))
@@ -18,7 +20,7 @@ def test_cervical_cancer_new_dataset():
 
 
 def test_datatypes():
-    X, y, df = parsing_helper("cervical_new", return_X_y=True)
+    X, y, df = parsing_helper("cervical_new")
     assert X.dtype == np.float32
     assert y.dtype == np.int8
     assert df.dtype == np.float32
