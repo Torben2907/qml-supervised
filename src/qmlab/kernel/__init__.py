333d2f55cce1378d337c15a5ac49ebd2f85c2884
Torben2907 <torben.heckes@hhu.de>
2024-11-12 19:42:20 +0100

    add to notebook

--- src/qmlab/kernel/__init__.py
+++ src/qmlab/kernel/__init__.py
@@ -1,3 +1,5 @@
 from .quantum_kernel import QuantumKernel
+from .trainable_kernel import TrainableQuantumKernel
+from .kernel_loss import SVCLoss, KTALoss
 
-__all__ = ["QuantumKernel"]
+__all__ = ["QuantumKernel", "TrainableQuantumKernel", "SVCLoss", "KTALoss"]
--- src/qmlab/plotting/__init__.py
+++ src/qmlab/plotting/__init__.py
@@ -1,4 +1,9 @@
 from .plot_data import plot_2d_data_with_train_test_split
 from .plot_model import plot_decision_boundaries
+from .plot_kernel import plot_kernel_heatmap
 
-__all__ = ["plot_2d_data_with_train_test_split", "plot_decision_boundaries"]
+__all__ = [
+    "plot_2d_data_with_train_test_split",
+    "plot_decision_boundaries",
+    "plot_kernel_heatmap",
+]
